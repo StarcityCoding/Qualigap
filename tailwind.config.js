5f29ddc41852b1ac42c9313f9fd9299333826df6
StarcityCoding <107433752+StarcityCoding@users.noreply.github.com>
2022-07-03 19:03:12 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
       spacing: {
         '2/3': '66.666667%',
       },
+      fontWeight: {
+        normal: 300,
+      },
+      fontFamily: {
+        'Poppins': "'Poppins', sans-serif",
+        'outfit': "'Outfit', sans-serif",
+        'Averia': "'Averia Sans Libre', cursive",
+        'Intolight': "'Shadows Into Light', cursive",
+      },
+      lineHeight: {
+        'leading': '2.5',
+        '11': '3.0rem',
+        '12': '3.5rem',
+      },
+      padding: {
+        '18': '4.5rem',
+      },
+   
     },
   },
   variants: {
